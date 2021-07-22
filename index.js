c744cf4580d8b4f9df9a53911abc07658e8b55d9
J4M35-L4 <josemanuelsimangojunior@gmail.com>
2021-07-22 17:27:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const strl= [];
+app.use(express.json());
+
+/*
+A representacao de numeros romanos obedece as seguintes regras
+1-Algarismos de menor ou igual valor à direita são somados ao algarismo de maior valor;
+2-Algarismos de menor valor à esquerda são subtraídos do algarismo de maior valor.
+*/
+
+
+function conversor(digito) {
+    switch (digito) {
+        case 'I':
+            return 1;
+            break;
+
+        case 'V':
+            return 5;
+            break;
+
+        case 'X':
+            return 10;
+            break;
+
+        case 'L':
+            return 50;
+            break;
+
+        case 'C':
+            return 100;
+            break;
+        case 'D':
+            return 500;
+            break;
+        case 'M':
+            return 1000;
+            break;
+
+        default: 
+            return -1;
+            break
+
+            
+    }
+}
+
+function pegar(str) {
+    var total=0;
+    
+    for (var i = 0; i < str.length; i++) {
+        strl[i]=conversor(str.charAt(i));
+      }
+
+    for (let index = 0; index < strl.length; index++) {
+        
+        if(i+1>strl.length){
+            if(strl[index]>=strl[index+1]){
+                total+=strl[index]
+            }else{
+                total+=(strl[index+1]-strl[index]);
+                index++;
+            }
+        }else{
+            total+=strl[index]
+        }
+        
+        
+    }
+    
+    return total;
+
+}
+
+
+app.post('/roman/:id',(req,res)=>{
+    var {id} = req.params;
+    var total = 0;
+    total = pegar(id);
+    res.send({
+        strl, total
+    });
+})
+
+app.listen(port, () => console.log(`Correndo o projeto`))
